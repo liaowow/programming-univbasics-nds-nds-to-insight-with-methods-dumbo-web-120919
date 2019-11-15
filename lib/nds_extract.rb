7654eeec74cb1593804169124d1f2a1d2da632c8
Annie Liao <liaowow@yahoo.com>
2019-11-15 00:28:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,7 +22,7 @@ def gross_for_director(director_data)
   movies_index = 0
   total = 0
   while movies_index < director_data[:movies].count do
-    total += director_data[:movies][movies_index][:worldwide_grosses]
+    total += director_data[:movies][movies_index][:worldwide_gross]
     movies_index += 1
   end
   total
